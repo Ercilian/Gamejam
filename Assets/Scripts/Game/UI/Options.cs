eccdc2137e103063f469459da21518b3c84e3b97
Ercilian <davidamunoz99@gmail.com>
2026-01-29 21:53:26 +0100

    Boton gameover

--- Assets/Scripts/Game/UI/Options.cs
+++ Assets/Scripts/Game/UI/Options.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class Options : MonoBehaviour
 {
 
     public GameObject OptionsPanel; // panel for options menu
     public GameObject GameOverPanel; // panel for game over menu
+    public Button mainmenubutton; // button to return to main menu from game over
 
 
     private void Start()
@@ -44,6 +46,8 @@ public class Options : MonoBehaviour
     public void GameOver()
     {
         GameOverPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(mainmenubutton.gameObject);
+
         Time.timeScale = 0f; // Pause the game
     }   
 
